13b3b7842f9dd0e9f519eb651e1885e2a6084513
zhangliming <1048655201@qq.com>
2018-09-20 10:50:10 +0800

    开发message组件

--- libs/index.js
+++ libs/index.js
@@ -6,14 +6,16 @@ import Button from './button'
 import Icon from './icon'
 import Collapse from './collapse'
 import CollapseItem from './collapse-item'
+import Message from './message'
 
 const components = [
-    Button, Icon, Collapse, CollapseItem
+    Button, Icon, Collapse, CollapseItem, Message
 ]
 
 const install = function (Vue) {
     if (install.installed) return
     components.map(component => Vue.component(component.name, component))
+    Vue.prototype.$message = Message
 }
 
 export default {
@@ -21,5 +23,6 @@ export default {
     Button,
     Icon,
     Collapse,
-    CollapseItem
+    CollapseItem,
+    Message
 }
---
+++ libs/message/Message.vue
@@ -0,0 +1,112 @@
+/**
+ * Created by zhangliming on 2018/9/15.
+ *
+ */
+
+<template>
+    <transition class="b-message-fade" mode="in-out">
+        <div
+            v-show="show"
+            class="b-message-wrapper"
+            :style="{ zIndex: zIndex }">
+            <div
+                class="b-message"
+                :style="{ background: bgColor }"
+                @mouseenter="enter"
+                @mouseleave="leave">
+                <img
+                    v-if="imgSrc"
+                    class="b-message-img"
+                    :src="imgSrc">
+                {{message}}
+                <button
+                    v-if="showClose"
+                    class="b-message-close"
+                    @click="close">
+                    ×
+                </button>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+// import info from '@/assets/info.svg'
+// import error from '@/assets/error.svg'
+// import success from '@/assets/success.svg'
+// import warning from '@/assets/warning.svg'
+// import closeImg from '@/assets/close.svg'
+
+export default {
+    name: 'b-message',
+    data () {
+        return {
+            show: false,
+            timer: null,
+            zIndex: 80,
+            closed: false,
+            closeImg: '',
+            onClose: null,
+            message: '',
+            type: 'info',
+            duration: 3000,
+            showClose: false
+        }
+    },
+    computed: {
+        imgSrc () {
+            let img = {
+                info: '',
+                error: '',
+                success: '',
+                warning: ''
+            }
+            return img[this.type]
+        },
+        bgColor () {
+            let colors = {
+                info: '#fff',
+                error: '#f44336',
+                success: '#1fca74',
+                warning: '#ff8f00'
+            }
+            return colors[this.type]
+        }
+    },
+    mounted () {
+        this.enter()
+        if (this.duration <= 0) {
+            this.showClose = true
+        }
+    },
+    watch: {
+        closed (val) {
+            this.show = false
+            this.$el.addEventListener('animationend', this.destroyElement)
+        }
+    },
+    methods: {
+        close () {
+            this.closed = true
+            if (typeof this.onClose === 'function') {
+                this.onClose(this)
+            }
+        },
+        destroyElement () {
+            this.$el.removeEventListener('transitionend', this.destroyElement)
+            this.$destroy(true)
+            this.$el.parentNode.removeChild(this.$el)
+        },
+        enter () {
+            this.timer = setTimeout(() => {
+                if (!this.closed) {
+                    this.close()
+                }
+            }, this.duration)
+        },
+        leave () {
+            clearTimeout(this.timer)
+        }
+    }
+}
+</script>
