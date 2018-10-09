16f035c4981bcc57f103555255a2939e6129caad
gaoguoqing <gaoguoqing@baiwang.com>
2018-10-09 11:02:19 +0800

    grid 初始化

---
+++ libs/grid/col.vue
@@ -0,0 +1,28 @@
+<template>
+    <div :class="classes" :style="styles">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+const prefixCls = 'b-col'
+
+export default {
+    name: 'col',
+    computed: {
+        classes () {
+            let classList = [
+                `${prefixCls}`,
+                {
+                    [`${prefixCls}-span-${this.span}`]: this.span
+                }
+            ]
+            return classList
+        },
+        styles () {
+            let style = {}
+            return style
+        }
+    }
+}
+</script>
---
+++ libs/grid/row.vue
@@ -0,0 +1,26 @@
+<template>
+    <div :class="rowClass" :style="rowStyle">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+const prefixCls = 'b-row'
+export default {
+    name: 'row',
+    computed: {
+        rowClass () {
+            return [
+                {
+                    [`${prefixCls}`]: !this.type,
+                    [`${prefixCls}-${this.type}`]: !!this.type
+                }
+            ]
+        },
+        rowStyle () {
+            let style = {}
+            return style
+        }
+    }
+}
+</script>
