03736d3c75dd0a7fa9b8dbfe192dfd759feb8c68
zhangliming <1048655201@qq.com>
2018-10-19 16:00:57 +0800

    调整组件

--- libs/message/index.js
+++ libs/message/index.js
@@ -75,12 +75,12 @@ types.forEach(type => {
     }
 })
 
-Message.install = function (Vue, options) {
-    if (options && options.name) {
-        Vue.prototype[`$${options.name}`] = Message
-    } else {
-        Vue.prototype.$message = Message
-    }
-}
+// Message.install = function (Vue, options) {
+//     if (options && options.name) {
+//         Vue.prototype[`$${options.name}`] = Message
+//     } else {
+//         Vue.prototype.$message = Message
+//     }
+// }
 
 export default Message
