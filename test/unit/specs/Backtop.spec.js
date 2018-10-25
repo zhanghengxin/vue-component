84482840af109ba680adc018c703d998e7d9fb9c
zenglingnan <zenglingnan@baiwang.com>
2018-10-25 19:29:45 +0800

    测试化用例代码提交

--- test/unit/specs/Backtop.spec.js
+++ test/unit/specs/Backtop.spec.js
@@ -1,18 +1,20 @@
 
-/*  前者一个测试套执行一次, 后者每个测试用例执行一次。*/
+/*  前者一个测试套执行一次, 后者每个测试用例执行一次。 */
 
-import {createVue, destroyVM, createTest} from '../utils'
+import {destroyVM, createTest} from '../utils'
+import Backtop from '&/components/backTop'
 
-// describe('breadTop.vue', () => { // describe相当于一个测试套，可以嵌套。
-//     let vm
-//     afterEach(() => {
-//         destroyVM(vm) // 清理环境或全局变量
-//     })
-//     it('height', () => { // it('tc name',() => {})是一个测试用例。
-//         vm = createTest(backtop, {
-//           height:300
-//         }, true)
-//         let buttonElm = vm.$el
-//         expect(buttonElm.style.height).to.equal('300px')
-//     })
-// })
+describe('Backtop.vue', () => { // describe相当于一个测试套，可以嵌套。
+    let vm
+    afterEach(() => {
+        destroyVM(vm) // 清理环境或全局变量
+    })
+    it('right', () => { // it('tc name',() => {})是一个测试用例。
+        vm = createTest(Backtop, {
+            right: 30
+        }, true)
+        let backTopEle = vm.$el
+        expect(backTopEle.style.right).to.equal('30px')
+    })
+
+})
