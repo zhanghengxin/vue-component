3a7a69f8c05fbf47b79991f194356ac1e2c23c58
lijiahang <lijiahang@baiwang.com>
2018-11-30 15:19:27 +0800

    修改props button tag

--- libs/components/drawer/drawer.vue
+++ libs/components/drawer/drawer.vue
@@ -1,8 +1,8 @@
 /*
  * @Author: lijiahang
  * @Date: 2018-11-12 10:51:51
- * @Last Modified by:   lijiahang
- * @Last Modified time: 2018-11-12 10:51:51
+ * @Last Modified by: lijiahang
+ * @Last Modified time: 2018-11-29 19:45:45
  */
 <template>
     <div :class="[prefixCls+'-root']" ref='mask'>
