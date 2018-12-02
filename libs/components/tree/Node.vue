a95b99b160f61b3892afeb191a8376e892ed2048
gaoguoqing <gaoguoqing@baiwang.com>
2018-12-02 17:52:21 +0800

    Tree 组件提交

---
+++ libs/components/tree/Node.vue
@@ -0,0 +1,179 @@
+<template>
+    <ul
+        :class="wrapCls"
+        v-show="!data.invisible"
+        @dragstart.stop="handleDragStart"
+        @dragover.stop="handleDragOver"
+        @drop.stop="handleDrop"
+        :nodeKey="data.nodeKey"
+        :draggable="draggable">
+        <li>
+            <span :class="arrowsCls">
+                <Icon v-if="showArrow" :type="arrowType" @on-click="expanded"></Icon>
+                <Icon v-if="showLoading" type="chushihua" :class="[prefixCls +'-loop']"></Icon>
+            </span>
+            <Checkbox
+                v-if="showCheckbox"
+                :value="data[defaultOpt.checkedKey]"
+                :indeterminate="data[defaultOpt.indeterminateKey]"
+                :disabled="data[defaultOpt.disabledKey]"
+                @click.native.prevent="handleCheck">
+            </Checkbox>
+            <span :class="nameCls" @click="selectData">{{ data[defaultOpt.nameKey] }}</span>
+            <tree-node
+                v-for="(item, key) in children"
+                v-if="data[defaultOpt.expandKey]"
+                :key="key"
+                :draggable="draggable"
+                :data="item"
+                :show-checkbox="showCheckbox"
+                :default-opt="defaultOpt">
+            </tree-node>
+        </li>
+    </ul>
+</template>
+<script>
+import { findComponentUpwards, findComponentUpward } from '../../utils/assist'
+import Checkbox from '../checkbox/Checkbox.vue'
+import Icon from '../icon/Icon.vue'
+import { prefix } from '../../utils/common'
+import Emitter from '../../mixins/emitter'
+
+const prefixCls = prefix + 'tree'
+
+export default {
+    name: 'tree-node',
+    components: {Checkbox, Icon},
+    mixins: [Emitter],
+    props: {
+        data: {
+            type: Object,
+            default () {
+                return {}
+            }
+        },
+        defaultOpt: {
+            default () {
+                return {
+                    childrenKey: 'children',
+                    nameKey: 'name',
+                    disabledKey: 'disabled',
+                    checkedKey: 'checked',
+                    expandKey: 'expand',
+                    selectedKey: 'selected',
+                    indeterminateKey: 'indeterminate'
+                }
+            }
+        },
+        showCheckbox: {
+            type: Boolean,
+            default: false
+        },
+        draggable: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data () {
+        return {
+            prefixCls: prefixCls
+        }
+    },
+    computed: {
+        wrapCls () {
+            return [
+                `${prefixCls}-children`
+            ]
+        },
+        arrowsCls () {
+            return [
+                `${prefixCls}-arrow`
+            ]
+        },
+        arrowType () {
+            return this.data[this.defaultOpt.expandKey] ? 'xiasanjiao' : 'yousanjiao'
+        },
+        showArrow () {
+            return (this.data[this.defaultOpt.childrenKey] && this.data[this.defaultOpt.childrenKey].length) || ('loading' in this.data && !this.data.loading)
+        },
+        nameCls () {
+            return [
+                `${prefixCls}-name`,
+                {
+                    [`${prefixCls}-name-selected`]: this.data[this.defaultOpt.selectedKey],
+                    [`${prefixCls}-name-draggable`]: this.draggable
+                }
+            ]
+        },
+        children () {
+            return this.data[this.defaultOpt.childrenKey]
+        },
+        showLoading () {
+            return 'loading' in this.data && this.data.loading
+        }
+    },
+    methods: {
+        expanded () {
+            const data = this.data
+            const defaultOpt = this.defaultOpt
+            // if (data[defaultOpt.disabledKey]) return // 禁用
+            if (data[defaultOpt.childrenKey] && data[defaultOpt.childrenKey].length) {
+                this.$set(this.data, defaultOpt.expandKey, !this.data[defaultOpt.expandKey])
+            }
+            // loading
+            if (!data[defaultOpt.childrenKey] || data[defaultOpt.childrenKey].length === 0) {
+                const tree = findComponentUpward(this, prefixCls)
+                if (tree && tree.loading && tree.loadMethod) {
+                    this.$set(data, 'loading', true)
+                    tree.loadMethod(data, children => {
+                        this.$set(data, 'loading', false)
+                        if (children.length) {
+                            this.$set(data, defaultOpt.childrenKey, children)
+                            this.$nextTick(() => this.expanded())
+                        }
+                    })
+                    return
+                }
+            }
+            // accordion
+            let parents = findComponentUpwards(this, prefixCls).map(item => {
+                return item.data
+            })
+            parents.push(data)
+            this.dispatch(prefix + 'tree', 'on-expand-change', {data: this.data, parents: parents})
+        },
+        selectData () {
+            const defaultOpt = this.defaultOpt
+            if (this.data[defaultOpt.disabled] || this.showCheckbox) return
+            this.dispatch(prefix + 'tree', 'on-selected-change', this.data.nodeKey)
+        },
+        handleCheck () {
+            const defaultOpt = this.defaultOpt
+            if (this.data[defaultOpt.disabledKey]) return
+            const changes = {
+                checked: !this.data[defaultOpt.checkedKey] && !this.data[defaultOpt.indeterminateKey],
+                nodeKey: this.data.nodeKey
+            }
+            this.dispatch(prefix + 'tree', 'on-check-change', changes)
+        },
+        handleDragStart (event) {
+            this.dispatch(prefix + 'tree', 'on-drag-start', {
+                event: event,
+                treeNode: this
+            })
+        },
+        handleDragOver (event) {
+            if ('draggable' in this.data && !this.data.draggable) return
+            this.dispatch(prefix + 'tree', 'on-drag-over', this)
+            event.preventDefault()
+        },
+        handleDrop (event) {
+            if ('draggable' in this.data && !this.data.draggable) return
+            this.dispatch(prefix + 'tree', 'on-drag-drop', {
+                event: event,
+                treeNode: this.data
+            })
+        }
+    }
+}
+</script>
--- libs/utils/assist.js
+++ libs/utils/assist.js
@@ -5,7 +5,6 @@ let findComponentUpward = function (context, componentName, componentNames) {
     } else {
         componentNames = componentName
     }
-
     let parent = context.$parent
     let name = parent.$options.name
     while (parent && (!name || componentNames.indexOf(name) < 0)) {
@@ -14,6 +13,30 @@ let findComponentUpward = function (context, componentName, componentNames) {
     }
     return parent
 }
+
+let findComponentUpwards = function (context, componentName) {
+    let parent = context.$parent
+    let name = parent.$options.name
+    let parents = []
+    while (parent && (!name || componentName !== name)) {
+        parents.push(parent)
+        parent = parent.$parent
+        if (parent) name = parent.$options.name
+    }
+    return parents
+}
+
+let findNearestComponent = (element, componentName) => {
+    let target = element
+    while (target && target.tagName !== 'BODY') {
+        if (target.__vue__ && target.__vue__.$options.name === componentName) {
+            return target.__vue__
+        }
+        target = target.parentNode
+    }
+    return null
+}
+
 function typeOf (obj) {
     const toString = Object.prototype.toString
     const map = {
@@ -35,7 +58,6 @@ function typeOf (obj) {
 function deepCopy (data) {
     const t = typeOf(data)
     let o
-
     if (t === 'array') {
         o = []
     } else if (t === 'object') {
@@ -43,7 +65,6 @@ function deepCopy (data) {
     } else {
         return data
     }
-
     if (t === 'array') {
         for (let i = 0; i < data.length; i++) {
             o.push(deepCopy(data[i]))
@@ -55,4 +76,5 @@ function deepCopy (data) {
     }
     return o
 }
-export {findComponentUpward, deepCopy}
+
+export { findComponentUpward, deepCopy, findComponentUpwards, findNearestComponent }
