76173f34faef4a4194fe73d1161c1ab3f34e8504
gaoguoqing <gaoguoqing@baiwang.com>
2019-03-12 18:48:36 +0800

    3.0

---
+++ libs/build/webpack.base.config.js
@@ -0,0 +1,105 @@
+/**
+ * 公共配置
+ * Created by gaoguoqing on 2018/12/10.
+ *
+ */
+const path = require('path')
+const webpack = require('webpack')
+const package = require('../package.json')
+
+function resolve (dir) {
+    return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        css: [
+                            'vue-style-loader',
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    sourceMap: true,
+                                },
+                            },
+                        ],
+                        sass: [
+                            'vue-style-loader',
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    sourceMap: true,
+                                },
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    sourceMap: true,
+                                },
+                            },
+                        ],
+                    },
+                    postLoaders: {
+                        html: 'babel-loader?sourceMap'
+                    },
+                    sourceMap: true,
+                }
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    sourceMap: true,
+                },
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                loaders: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            sourceMap: false,
+                        },
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: false,
+                        },
+                    },
+                    {
+                        loader: '\'autoprefixer-loader\'',
+                    }
+                ]
+            },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader?limit=8192'
+            },
+            {
+                test: /\.(html|tpl)$/,
+                loader: 'html-loader'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.vue'],
+        alias: {
+            '@': resolve('../src'),
+            '&': resolve('./'),
+            'vue': 'vue/dist/vue.esm.js'
+        }
+    },
+    plugins: [
+        new webpack.optimize.ModuleConcatenationPlugin(),
+        new webpack.DefinePlugin({
+            'process.env.VERSION': `'${package.version}'`
+        })
+    ]
+}
