4515198844315064578af958fbb75a45578922e7
hanshuai <hanshuai@baiwang.com>
2019-04-15 11:33:09 +0800

    drawer添加可拖拽功能

--- libs/components/drawer/drawer.vue
+++ libs/components/drawer/drawer.vue
@@ -1,66 +1,305 @@
-/*
- * @Author: lijiahang
- * @Date: 2018-11-12 10:51:51
- * @Last Modified by: hanshuai@baiwang.com
- * @Last Modified time: 2019-01-09 17:22:26
- */
 <template>
-    <div :class="[prefixCls+'-root']" ref='mask'>
-        <div :class="mask" v-show="isshow" @click="maskclick"></div>
-        <transition :name="drawer">
-            <div v-show="isshow" :class="drawer">
-                <slot name='drawer'>
-                    <div :class="[prefixCls+'-title']">
-                        <p>{{title}}</p>
-                    </div>
-                    <div :class="[prefixCls+'-body']">
-                        <slot/>
-                    </div>
-                </slot>
+    <div v-transfer-dom :data-transfer="transfer">
+        <transition name="fade">
+            <div
+                :class="maskClasses"
+                :style="maskStyle"
+                v-show="visible"
+                v-if="mask"
+                @click="handleMask">
             </div>
         </transition>
+        <div :class="wrapClasses" @click="handleWrapClick">
+            <transition :name="'move-' + placement">
+                <div :class="classes" :style="mainStyles" v-show="visible">
+                    <div ref="content" :class="contentClasses">
+                        <a :class="[prefixCls + '-close']" v-if="closable" @click="close">
+                            <slot name="close">
+                                <Icon type="quxiao-guanbi-shanchu" size="18"></Icon>
+                            </slot>
+                        </a>
+                        <div :class="[prefixCls + '-header']" v-if="showHead">
+                            <slot name="header">
+                                <div :class="[prefixCls + '-header-inner']">
+                                    {{ title }}
+                                </div>
+                            </slot>
+                        </div>
+                        <div :class="[prefixCls + '-body']" :style="styles">
+                            <slot></slot>
+                        </div>
+                    </div>
+                    <div
+                        :class="dragClasses"
+                        v-if="draggable"
+                        @mousedown="handleTriggerMousedown">
+                        <slot name="trigger">
+                            <div :class="[prefixCls + '-drag-move-trigger']">
+                                <div :class="[prefixCls + '-drag-move-trigger-point']">
+                                    <i :key="item" v-for="item in drags"></i>
+                                 </div>
+                            </div>
+                        </slot>
+                    </div>
+                </div>
+            </transition>
+        </div>
     </div>
 </template>
 <script>
-import { prefix } from '../../utils/common'
+    import Icon from '../icon'
+    import { findBrothersComponents, findComponentUpward } from '../../utils/assist'
+    import TransferDom from '../../utils/directives/transfer-dom'
+    import Emitter from '../../mixins/emitter'
+    import { oneOf, prefix } from '../../utils/common'
+    import ScrollbarMixins from '../modal/mixins/scrollbar'
+    import { on, off } from '../../utils/dom'
+    const prefixCls = `${prefix}drawer`
 
-const prefixCls = prefix + 'drawer'
-export default {
-    name: prefixCls,
-    data () {
-        return {
-            prefixCls: prefixCls
-        }
-    },
-    props: {
-        local: {
-            type: String,
-            default: 'right'
+    export default {
+        name: prefixCls,
+        mixins: [ Emitter, ScrollbarMixins ],
+        components: { Icon },
+        directives: { TransferDom },
+        props: {
+            value: {
+                type: Boolean,
+                default: false
+            },
+            title: String,
+            width: {
+                type: [Number, String],
+                default: 256
+            },
+            closable: {
+                type: Boolean,
+                default: true
+            },
+            maskClosable: {
+                type: Boolean,
+                default: true
+            },
+            mask: {
+                type: Boolean,
+                default: true
+            },
+            maskStyle: Object,
+            styles: Object,
+            scrollable: {
+                type: Boolean,
+                default: false
+            },
+            placement: {
+                validator (value) {
+                    return oneOf(value, ['left', 'right'])
+                },
+                default: 'right'
+            },
+            className: String,
+            inner: {
+                type: Boolean,
+                default: false
+            },
+            transfer: {
+                type: Boolean,
+                default: true
+            },
+            draggable: {
+                type: Boolean,
+                default: false
+            },
+            beforeClose: Function
         },
-        title: {
-            type: String,
-            default: '百望股份'
+        data () {
+            return {
+                prefixCls: prefixCls,
+                visible: this.value,
+                wrapShow: false,
+                showHead: true,
+                canMove: false,
+                dragWidth: this.width,
+                wrapperWidth: this.width,
+                wrapperLeft: 0,
+                minWidth: 256,
+                drags: [1, 2, 3, 4, 5, 6]
+            }
         },
-        isshow: {
-            type: Boolean,
-            default: false
-        }
-    },
-    computed: {
-        drawer () {
-            return `${prefixCls}-${this.local}`
+        computed: {
+            wrapClasses () {
+                return [
+                    `${prefixCls}-wrap`,
+                    {
+                        [`${prefixCls}-hidden`]: !this.wrapShow,
+                        [`${this.className}`]: !!this.className,
+                        [`${prefixCls}-no-mask`]: !this.mask,
+                        [`${prefixCls}-wrap-inner`]: this.inner,
+                        [`${prefixCls}-wrap-dragging`]: this.canMove
+                    }
+                ]
+            },
+            mainStyles () {
+                let style = {}
+                const width = parseInt(this.dragWidth)
+                const styleWidth = {
+                    width: width <= 100 ? `${width}%` : `${width}px`
+                }
+                Object.assign(style, styleWidth)
+                return style
+            },
+            contentClasses () {
+                return [
+                    `${prefixCls}-content`,
+                    {
+                        [`${prefixCls}-content-no-mask`]: !this.mask
+                    }
+                ]
+            },
+            classes () {
+                return [
+                    `${prefixCls}`,
+                    `${prefixCls}-${this.placement}`,
+                    {
+                        [`${prefixCls}-no-header`]: !this.showHead,
+                        [`${prefixCls}-inner`]: this.inner
+                    }
+                ]
+            },
+            maskClasses () {
+                return [
+                    `${prefixCls}-mask`,
+                    {
+                        [`${prefixCls}-mask-inner`]: this.inner
+                    }
+                ]
+            },
+            dragClasses () {
+                return [
+                    `${prefixCls}-drag`,
+                    {
+                        [`${prefixCls}-drag-left`]: this.placement === 'left'
+                    }
+                ]
+            }
         },
-        mask () {
-            return [`${prefixCls}-mask`]
+        methods: {
+            close () {
+                if (!this.beforeClose) {
+                    return this.handleClose()
+                }
+                const before = this.beforeClose()
+                if (before && before.then) {
+                    before.then(() => {
+                        this.handleClose()
+                    })
+                } else {
+                    this.handleClose()
+                }
+            },
+            handleClose () {
+                this.visible = false
+                this.$emit('input', false)
+                this.$emit('on-close')
+            },
+            handleMask () {
+                if (this.maskClosable && this.mask) {
+                    this.close()
+                }
+            },
+            handleWrapClick (event) {
+                const className = event.target.getAttribute('class')
+                if (className && className.indexOf(`${prefixCls}-wrap`) > -1) {
+                    this.handleMask()
+                }
+            },
+            handleMousemove (event) {
+                if (!this.canMove || !this.draggable) return
+                // 更新容器宽度和距离左侧页面距离，如果是window则距左侧距离为0
+                this.handleSetWrapperWidth()
+                const left = event.pageX - this.wrapperLeft
+                // 如果抽屉方向为右边，宽度计算需用容器宽度减去left
+                let width = this.placement === 'right' ? this.wrapperWidth - left : left
+                // 限定最小宽度
+                width = Math.max(width, parseFloat(this.minWidth))
+                event.atMin = width === parseFloat(this.minWidth)
+                // 如果当前width不大于100，视为百分比
+                if (width <= 100) width = (width / this.wrapperWidth) * 100
+                this.dragWidth = width
+                this.$emit('on-resize-width', parseInt(this.dragWidth))
+            },
+            handleSetWrapperWidth () {
+                const { width, left } = this.$el.getBoundingClientRect()
+                // console.log('width: %o\n left: %o\n this: %o\n', width, left, this)
+                this.wrapperWidth = width
+                this.wrapperLeft = left
+            },
+            handleMouseup () {
+                if (!this.draggable) return
+                this.canMove = false
+            },
+            handleTriggerMousedown () {
+                this.canMove = true
+                // 防止鼠标选中抽屉中文字，造成拖动trigger触发浏览器原生拖动行为
+                window.getSelection().removeAllRanges()
+            }
+        },
+        mounted () {
+            if (this.visible) {
+                this.wrapShow = true
+            }
+            let showHead = true
+            if (this.$slots.header === undefined && !this.title) {
+                showHead = false
+            }
+            this.showHead = showHead
+            on(document, 'mousemove', this.handleMousemove)
+            on(document, 'mouseup', this.handleMouseup)
+            this.handleSetWrapperWidth()
+        },
+        beforeDestroy () {
+            off(document, 'mousemove', this.handleMousemove)
+            off(document, 'mouseup', this.handleMouseup)
+            this.removeScrollEffect()
+        },
+        watch: {
+            value (val) {
+                this.visible = val
+            },
+            visible (val) {
+                if (val === false) {
+                    this.timer = setTimeout(() => {
+                        this.wrapShow = false
+                        const brotherDrawers = findBrothersComponents(this, `${prefix}drawer`) || []
+                        const parentDrawers = findComponentUpward(this, `${prefix}drawer`) || []
+
+                        const otherDrawers = [].concat(brotherDrawers).concat(parentDrawers)
+                        const isScrollDrawer = otherDrawers.some(item => item.visible && !item.scrollable)
+                        if (!isScrollDrawer) {
+                            this.removeScrollEffect()
+                        }
+                    }, 300)
+                } else {
+                    if (this.timer) clearTimeout(this.timer)
+                    this.wrapShow = true
+                    if (!this.scrollable) {
+                        this.addScrollEffect()
+                    }
+                }
+
+                this.broadcast(`${prefix}table`, 'on-visible-change', val)
+                this.$emit('on-visible-change', val)
+            },
+            scrollable (val) {
+                if (!val) {
+                    this.addScrollEffect()
+                } else {
+                    this.removeScrollEffect()
+                }
+            },
+            title (val) {
+                if (this.$slots.header === undefined) {
+                    this.showHead = !!val
+                }
+            }
         }
-    },
-    methods: {
-        maskclick () {
-            this.$emit('drawerchange')
-        }
-    },
-    mounted () {
-        document.body.appendChild(this.$refs.mask)
     }
-}
 </script>
--- src/documents/basicDocments/drawer.md
+++ src/documents/basicDocments/drawer.md
@@ -1,211 +1,266 @@
 # Drawer 抽屉
-常见的抽屉组件
-### 概述
-抽屉从父窗体边缘滑入，覆盖住部分父窗体内容。用户在抽屉内操作时不必离开当前任务，操作完成后，可以平滑地回到到原任务。
 
-#### 代码演示
-<script>
-        export default {
-          data () {
-            return { 
-                local1:'bottom',
-                local2:'left',
-                localt:'top',
-                localw:'left',
-                isShow:false,
-                isShowtwo:false,
-                isShoww:false,
-                isShowt:false,
-                
-            }
-          },
-          methods: {
-            change () {
-                this.isShow =!this.isShow
-            },
-            changetwo () {
-                this.isShowtwo =!this.isShowtwo
-            },
-            change1(){
-                this.isShow= !this.isShow
-            },
-            changew () {
-                   this.isShoww =!this.isShoww
-            },
-            changebw(){
-                this.isShoww =!this.isShoww
-            },
-            changet(){
-                this.isShowt =!this.isShowt
-            },
-            changebt(){
-                 this.isShowt =!this.isShowt
-            }
-          }
-        }
-</script>
+### 基础抽屉
 
 <div class="example-box">
-        <div>
-            <b-button type="primary"  @on-click="change" >默认抽屉</b-button>
-               <b-drawer :isshow="isShow" @drawerchange='change1'>
-                 <p>产品研发中心</p>
-                 <p>移动前端技术部</p>
-                 <p>业务一组</p>
-                 <p>李佳航</p>
-                </b-drawer>
-        </div>
+    <div>
+        <b-button type="primary"  @on-click="value1 = true">默认抽屉</b-button>
+        <b-drawer title="Basic Drawer" closable v-model="value1">
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
     </div>
 
 ::: code
 ```html
     <div>
-        <b-button  @on-click="change" >默认抽屉</b-button>
-        <b-drawer :isshow="isShow" @drawerchange='close'>
-            <p>产品研发中心</p>
-            <p>移动前端技术部</p>
-            <p>业务一组</p>
-            <p>李佳航</p>
-        </b-drawer>
-    </div>
-    <script>
-        export default {
-          data () {
-            return { 
-                isShow:false,
-            }
-          },
-          methods: {
-            change () {
-                this.isShow =!this.isShow
-            },
-            close(){
-                this.isShow= !this.isShow
-            }
-          }
-        }
-    </script>
-:::
-
-<div>
-<b-button type="success"  @on-click="changetwo" >底部抽屉</b-button>
-<b-drawer :local='local1' :isshow="isShowtwo" @drawerchange='changetwo'>
-    <p>产品研发中心</p>
-    <p>移动前端技术部</p>
-<p>业务一组</p>
-<p>李佳航</p>
-</b-drawer>
-</div>
-
-::: code
-```html
-    <div>
-        <b-button type="normal" @on-click="change">默认抽屉</b-button>
-        <b-drawer :local='local' :isshow="isShow" @drawerchange='close'>
-            <p>产品研发中心</p>
-            <p>移动前端技术部</p>
-            <p>业务一组</p>
-            <p>李佳航</p>
+        <b-button type="primary"  @on-click="value1 = true" >默认抽屉</b-button>
+        <b-drawer title="Basic Drawer" closable v-model="value1">
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
         </b-drawer>
     </div>
     <script>
         export default {
             data () {
-            return { 
-                local:'bottom',
-                isShow:false,
-            }
-            },
-            methods: {
-            change () {
-                this.isShow =!this.isShow
-            },
-            close(){
-                this.isShow= !this.isShow
-            }
+                return { 
+                    value1 :false,
+                }
             }
         }
     </script>
+```
 :::
-<div>
-<b-button type="warning"  @on-click="changew" >左边抽屉</b-button>
-<b-drawer :local='localw' :isshow="isShoww" @drawerchange='changebw'>
-    <p>产品研发中心</p>
-    <p>移动前端技术部</p>
-<p>业务一组</p>
-<p>李佳航</p>
-</b-drawer>
 </div>
+
+### 左侧滑出
+
+<div class="example-box">
+    <div>
+        <b-button type="primary"  @on-click="value2 = true" >左侧滑出</b-button>
+        <b-drawer title="Basic Drawer" placement="left" closable v-model="value2">
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+        </b-drawer>
+    </div>
 
 ::: code
 ```html
     <div>
-        <b-button type="warning" @on-click="change">左边抽屉</b-button>
-        <b-drawer :local='local' :isshow="isShow" @drawerchange='close'>
-            <p>产品研发中心</p>
-            <p>移动前端技术部</p>
-            <p>业务一组</p>
-            <p>李佳航</p>
+        <b-button type="primary"  @on-click="value2 = true" >左侧滑出</b-button>
+        <b-drawer title="Basic Drawer" placement="left" closable v-model="value2">
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
         </b-drawer>
     </div>
     <script>
         export default {
             data () {
-            return { 
-                local:'left',
-                isShow:false,
-            }
-            },
-            methods: {
-            change () {
-                this.isShow =!this.isShow
-            },
-            close(){
-                this.isShow= !this.isShow
-            }
+                return { 
+                    value2 :false,
+                }
             }
         }
     </script>
-
+```
 :::
-<div>
-    <b-button type="error"  @on-click="changet" >上边抽屉</b-button>
-    <b-drawer :local='localt' :isshow="isShowt" @drawerchange='changebt'>
-        <p>产品研发中心</p>
-        <p>移动前端技术部</p>
-        <p>业务一组</p>
-        <p>李佳航</p>
-    </b-drawer>
 </div>
+
+### 按钮关闭
+
+<div class="example-box">
+    <div>
+        <b-button type="primary"  @on-click="value3 = true" >按钮关闭</b-button>
+        <b-drawer title="Basic Drawer" :mask-closable="false" closable v-model="value3">
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <div class='drawer-footer'>
+                <b-button @on-click="value3 = false">取消</b-button>
+            </div>
+        </b-drawer>
+    </div>
 
 ::: code
 ```html
     <div>
-        <b-button type="warning" @on-click="change">左边抽屉</b-button>
-        <b-drawer :local='local' :isshow="isShow" @drawerchange='close'>
-            <p>产品研发中心</p>
-            <p>移动前端技术部</p>
-            <p>业务一组</p>
-            <p>李佳航</p>
+        <b-button type="primary"  @on-click="value3 = true" >按钮关闭</b-button>
+        <b-drawer title="Basic Drawer" :mask-closable="false" closable v-model="value3">
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <div class='drawer-footer'>
+                <b-button @on-click="value3 = false">取消</b-button>
+            </div>
         </b-drawer>
     </div>
     <script>
         export default {
             data () {
-            return { 
-                local:'top',
-                isShow:false,
-            }
-            },
-            methods: {
-            change () {
-                this.isShow =!this.isShow
-            },
-            close(){
-                this.isShow= !this.isShow
-            }
+                return { 
+                    value3 :false,
+                }
             }
         }
     </script>
+    <style>
+        .drawer-footer {
+            width: 100%;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            border-top: 1px solid #e8e8e8;
+            padding: 10px 16px;
+            text-align: right;
+            background: #fff;
+        }
+    </style>
+```
+:::
+</div>
 
+### 多层抽屉
 
+<div class="example-box">
+    <div>
+        <b-button type="primary" @on-click="value5 = true">多层抽屉</b-button>
+        <b-drawer title="第一层" width="512" :closable="false" v-model="value5">
+            <b-button @on-click="value6 = true" type="primary">第二层</b-button>
+        </b-drawer>
+        <b-drawer title="第二层" :closable="false" v-model="value6">
+            这个是第二层 Drawer
+        </b-drawer>
+    </div>
+
+::: code
+```html
+    <div>
+        <b-button type="primary" @on-click="value5 = true">多层抽屉</b-button>
+        <b-drawer title="第一层" width="512" :closable="false" v-model="value5">
+            <b-button @on-click="value6 = true" type="primary">第二层</b-button>
+        </b-drawer>
+        <b-drawer title="第二层" :closable="false" v-model="value6">
+            这个是第二层 Drawer
+        </b-drawer>
+    </div>
+    <script>
+        export default {
+            data () {
+                return { 
+                    value5 :false,
+                    value6: false
+                }
+            }
+        }
+    </script>
+    <style>
+        .drawer-footer {
+            width: 100%;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            border-top: 1px solid #e8e8e8;
+            padding: 10px 16px;
+            text-align: right;
+            background: #fff;
+        }
+    </style>
+```
+:::
+</div>
+
+### 拖拽
+
+<div class="example-box">
+    <div>
+        <b-button type="primary"  @on-click="value7 = true">拖拽</b-button>
+        <b-drawer title="Basic Drawer" draggable closable v-model="value7">
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+        </b-drawer>
+    </div>
+
+::: code
+```html
+    <div>
+        <b-button type="primary"  @on-click="value7 = true" >拖拽</b-button>
+        <b-drawer title="Basic Drawer" draggable closable v-model="value7">
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+            <p>Some contents...</p>
+        </b-drawer>
+    </div>
+    <script>
+        export default {
+            data () {
+                return { 
+                    value7 :false,
+                }
+            }
+        }
+    </script>
+```
+:::
+</div>
+
+<script>
+    export default {
+        data () {
+            return { 
+                value1: false,
+                value2: false,
+                value3: false,
+                value5: false,
+                value6: false,
+                value7: false
+            }
+        },
+    }
+</script>
+<style>
+.drawer-footer {
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    border-top: 1px solid #e8e8e8;
+    padding: 10px 16px;
+    background: #fff;
+}
+</style>
+
+### props
+
+| 参数      | 说明    | 类型      | 可选值       | 默认值   |
+|---------- |-------- |---------- |-------------  |-------- |
+| value   | 抽屉是否显示，可使用 v-model 双向绑定数据 | Boolean  | `true` `false` | false | 
+| title  | 抽屉标题，如果使用 slot 自定义了页头，则 title 无效 | String |
+| width   | 抽屉宽度。当其值不大于 100 时以百分比显示，大于 100 时为像素 | Number/String  | | 256 |
+| closable  | 是否显示右上角的关闭按钮 | Boolean  |  `true` `false`   |   false   |
+| mask-closable  | 是否允许点击遮罩层关闭    | Boolean   |    |  true     |
+| mask  | 是否显示遮罩层   | Boolean  |  |  true    |
+| mask-style  | 遮罩层样式 | Object  |
+| styles    | 抽屉中间层的样式  | Object   |
+| scrollable    | 页面是否可以滚动  | Boolean   |  |    false   |
+| palcement    | 抽屉的方向，可选值为 left 或 right  | String   |  `left` `right`  |   right   |
+| class-name    | 设置抽屉容器.b-drawer-wrap的类名  | String   |
+| draggable | 是否开启拖拽调整宽度 | Boolean | `true` `false` | false |
+
+### events
+
+| 事件名	      | 说明	    | 返回值 |
+|-------------|---------|----------|
+| on-close    | 关闭抽屉时触发    |   |
+| on-visible-change  | 显示状态发生变化时触发 | true/false |
+
+### slot
+| name	      | 说明	    |
+|-------------|---------|
+| header    | 自定义标题栏   |
+| close   | 自定义右上角关闭内容 |
+| trigger   | 自定义调整宽度节点  | - |
+| | 抽屉主体内容 |
